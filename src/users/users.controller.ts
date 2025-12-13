b6942d976cda004bc28e9e74d7581a51d51e024a
Muhammad Talha <mtalha2k1@gmail.com>
2025-12-13 18:10:15 +0500

    validation, db and prisma

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,8 +7,12 @@ import {
   Patch,
   Post,
   Query,
+  ParseIntPipe,
+  ValidationPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users') // a decorator. its saying that it will handle the /users route. e.g => localhost:3000/users
 export class UsersController {
@@ -20,37 +24,30 @@ export class UsersController {
   }
 
   @Get(':id') //GET /users/:id
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
   }
 
   @Post() //POST /users
   create(
-    @Body()
-    user: {
-      name: string;
-      email: string;
-      role: 'INTERN' | 'ADMIN' | 'ENGINEER';
-    },
+    @Body(ValidationPipe)
+    createuserDto: CreateUserDto,
   ) {
-    return this.usersService.create(user);
+    return this.usersService.create(createuserDto);
   }
 
   @Patch(':id') // PATCH /users/:id
   update(
-    @Param('id') id: string,
-    @Body()
-    userUpdate: {
-      name?: string;
-      email?: string;
-      role?: 'INTERN' | 'ADMIN' | 'ENGINEER';
-    },
+    @Param('id', ParseIntPipe)
+    id: number,
+    @Body(ValidationPipe)
+    updateUserDto: UpdateUserDto,
   ) {
-    return this.usersService.update(+id, userUpdate);
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id') //DELETE /users/:id
-  delete(@Param('id') id: string) {
-    return this.usersService.delete(+id);
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.delete(id);
   }
 }
